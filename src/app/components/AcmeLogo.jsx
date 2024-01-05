70f63df4c216b1c2c5da26191c8947431e71156e
weldxit <subhransu.weldxit@gmail.com>
2024-01-05 15:15:41 +0530

    next ui

---
+++ src/app/components/AcmeLogo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+export const AcmeLogo = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+    <path fill="currentColor" d="M12 2L1 9v13h6v-7h4v7h6V9L12 2zM11 14h-2v-2h2v2zm4 0h-2v-2h2v2z"/>
+  </svg>
+  
+);
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import { Providers } from './providers'
+import Navbars from './components/Navbar'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Providers>
+        <Navbars>
+          {children}
+          </Navbars>
+        </Providers>
+      </body>
     </html>
   )
 }
